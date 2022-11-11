56f400120d532ac8b812e92c8fcab17b08555386
tr15tan01 <t.varamashvili@gmail.com>
2022-11-11 23:41:39 +0400

    added checks and modal

--- src/components/NavTabs.tsx
+++ src/components/NavTabs.tsx
@@ -1,6 +1,6 @@
 // import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
-import { CustomAccordion, ProgressBar } from "./index";
+import { CustomAccordion, ProgressBar, ModalComponent } from "./index";
 
 function NavTabs() {
   return (
--- src/components/SwitchComponent.tsx
+++ src/components/SwitchComponent.tsx
@@ -1,10 +1,29 @@
 import Form from "react-bootstrap/Form";
 import Container from "react-bootstrap/Container";
+import { useEffect, useLayoutEffect, useState } from "react";
+import { ModalComponent } from "./index";
 
 export function SwitchComponent() {
+  const [isChecked, setIsChecked] = useState<boolean>(false);
+
   const checkHandler = () => {
-    console.log("checked");
+    setIsChecked(!isChecked);
+    const item = localStorage.getItem("checked");
+    localStorage.setItem("checked", isChecked.toString());
+    console.log({ item });
+    console.log({ isChecked });
   };
+
+  useEffect(() => {
+    console.log("effect");
+    const item = localStorage.getItem("checked");
+    setIsChecked(!!item);
+    console.log(item);
+  }, []);
+
+  useLayoutEffect(() => {
+    console.log("layoutEffecg");
+  });
 
   return (
     <Container className="m-2">
@@ -14,6 +33,7 @@ export function SwitchComponent() {
           id="custom-switch"
           label="Check this switch of you have completed task one"
           onChange={checkHandler}
+          checked={isChecked}
         />
         <hr />
         <Form.Check
@@ -24,6 +44,7 @@ export function SwitchComponent() {
         />
         <hr />
       </Form>
+      <ModalComponent addTask={() => console.log("from navtans")} />
     </Container>
   );
 }
